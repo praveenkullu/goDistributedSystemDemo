b387c47779ffd1002a80a96dd75626888224e3b5
praveen <praveen.kullu@firewires.in>
2025-11-10 03:27:32 -0500

    added instructions and updated auto_test.sh

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # goDistributedSystemDemo
+
+
+Build the view service:
+```go build -o ./bin/viewServer ./view/view_server.go```
+
+Run the View service:
+```./bin/viewServer [args]```
+
+View service execution args:
+
+    ./bin/viewServer \
+	    -addr		- address for the view server, localhost:8000 (default)
+
+
+
+Build the kv server:
+```go build -o ./bin/kvServer ./kv_server_main/kv_server_main.go```
+
+Run the kv server:
+```./bin/kvServer [args]```
+
+KV server execution ags:
+
+    ./bin/kvServer \
+	    -vs				- address of the view service, localhost:8000 (default)
+	    -addr			- address of the server(kv server), localhost:8001 (default)
+  
+
+Build the client:
+```go build -o ./bin/client ./client_main/client.go```
+
+Run the client:
+```./bin/client [args]```
+
+KV server execution ags:
+
+    ./bin/client \
+	    -vs			- address of the view service, localhost:8000 (default)
+	    -op			- operation "put" or "get"
+	    -key		- key of the operation
+	    -value		- value of the key
+	    -ops		- "op1, op2, op3", ops of sequence
+	    -keys		- "key1, key2, key3", keys of the sequence of operations
+	    -values		- "value1, value2, value3", values of the sequence of operations
+
+Following should be the squence to deploy:
+
+    #In terminal #1
+    ./bin/viewServer 
+    
+    #In terminal #2
+    ./bin/kvServer
+     
+    #In terminal #3
+    ./bin/client -ops "put,get" -keys "key1,key1" -values "1,x"
+
+
+## Run the automated test (Report Requirement)
+run the following command:
+```./auto_test.sh | tee ./log/logs.txt```
+
+review and verify the logs.txt
--- auto_test.sh
+++ auto_test.sh
@@ -6,71 +6,77 @@ touch ./log/logs.txt
 echo "Starting automated test..." > ./log/logs.txt
 
 # start the view service
+printf "%${TERMINAL_WIDTH}s\n" "[Started View Service]"
 go run view/view_server.go &
 VS_PID=$!
 
-printf "%${TERMINAL_WIDTH}s\n" "Started View Service with PID $VS_PID"
 # echo "Started View Service with PID $VS_PID"
 
 sleep $WAIT_TIME
 
 # start a kv server 1
+printf "%${TERMINAL_WIDTH}s\n" "[Started KV Server 1]"
 go run kv_server_main/kv_server_main.go -addr="localhost:8001" -vs="localhost:8000" &
 KV1_PID=$!
-echo "Started KV Server 1 with PID $KV1_PID"
 
 
 # start the client and make a put operation A: 1
 sleep $WAIT_TIME
+printf "%${TERMINAL_WIDTH}s\n" "[client put 'a', get 'a']"
 go run client_main/client.go -vs="localhost:8000" -ops="put,get" -keys="a,a" -values="1,x"  
 
 sleep $WAIT_TIME
 # start another kv server
+printf "%${TERMINAL_WIDTH}s\n" "[Started KV Server 2]"
 go run kv_server_main/kv_server_main.go -addr="localhost:8002" -vs="localhost:8000"  &
 KV2_PID=$!
-echo "Started KV Server 2 with PID $KV2_PID"
 
 # start the client and make a put operation B: 2
 sleep $WAIT_TIME
+printf "%${TERMINAL_WIDTH}s\n" "[client put 'b', get 'b']"
 go run client_main/client.go -vs="localhost:8000" -op="put,get" -keys="b,b" -values="2,x"  
 
 # kill kv server 1
 sleep $WAIT_TIME
 kill $KV1_PID
-echo "Killed KV Server 1 with PID $KV1_PID"
+printf "%${TERMINAL_WIDTH}s\n" "[Killed KV Server 1]"
 
 # client get operation for key A
+printf "%${TERMINAL_WIDTH}s\n" "[client get 'a']"
 go run client_main/client.go -vs="localhost:8000" -op="get" -key="a" 
 
 # start server 3
+printf "%${TERMINAL_WIDTH}s\n" "[Started KV Server 3]"
 go run kv_server_main/kv_server_main.go -addr="localhost:8003" -vs="localhost:8000"  &
 KV3_PID=$!
-echo "Started KV Server 3 with PID $KV3_PID"
 #wait for state transfer
 sleep $TRANSFER_TIME
 
 # kill kv server 2
 kill $KV2_PID
-echo "Killed KV Server 2 with PID $KV2_PID"
+printf "%${TERMINAL_WIDTH}s\n" "[Killed KV Server 2]"
 
 # client get operation for key A
 sleep $WAIT_TIME
+printf "%${TERMINAL_WIDTH}s\n" "[client get 'a']"
 go run client_main/client.go -vs="localhost:8000" -op="get" -key="a" 
 
-#cleanup
-echo "Cleaning up..."
-kill $KV3_PID
-kill $VS_PID
-# In case any process is still running on port 8000, 8001, 8002, or 8003, kill them
-# kill -9 $(sudo lsof -t -i:8000)
+echo "[Starting Auto verification]"
 
-echo "Starting Auto verification..."
+# ger count of "Get(a) = 1" in logs.txt is equal to 3
+grep -c "Get(a) = 1" ./log/logs.txt | grep -q "^3$"
 
-grep "Get(a) = 1" ./log/logs.txt > /dev/null
 if [ $? -eq 0 ]; then
     echo "Test Passed: Key 'a' has correct value '1'"
 else
     echo "Test Failed: Key 'a' does not have correct value '1'"
 fi
 
-exit 0
+#cleanup
+printf "%${TERMINAL_WIDTH}s\n" "[Cleaning up]"
+kill $KV3_PID
+kill $VS_PID
+# In case any process is still running on port 8000, 8001, 8002, or 8003, kill them
+# kill -9 $(sudo lsof -t -i:8000)
+
+echo "Automated test completed." >> ./log/logs.txt
